676bb2d25df44fc46c7b1fae09083068ac5a58ca
pisyatvtorie <acereng1337@mail.ru>
2024-02-14 16:50:39 +0300

    complete

--- 27/Source.cpp
+++ 27/Source.cpp
@@ -184,19 +184,83 @@ double square()
 	return answer;
 }
 
+double degree3()
+{
+	system("cls");
+	double fnum;
+	double answer = 0;
+	cout << "Введите число: "; cin >> fnum;
+	system("cls");
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore();
+	}
+	else
+	{
+		answer = pow(fnum, 3);
+	}
+	system("cls");
+	cout << "Ответ: " << answer; cin >> fnum;
+	return answer;
+}
+
+double sinus() 
+{
+	system("cls");
+	double num;
+	double answer = 0;
+	cout << "Введите число: "; cin >> num;
+	system("cls");
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore();
+	}
+	else
+	{
+		answer = sin(num);
+	}
+	system("cls");
+	cout << "Ответ: " << answer; cin >> num;
+	return answer;
+}
+
+double cosinus()
+{
+	system("cls");
+	double num;
+	double answer = 0;
+	cout << "Введите число: "; cin >> num;
+	system("cls");
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore();
+	}
+	else
+	{
+		answer = cos(num);
+	}
+	system("cls");
+	cout << "Ответ: " << answer; cin >> num;
+	return answer;
+}
+
+
 int main() {
 	setlocale(LC_ALL, "RUS");
 
 	cout << "\tКАЛЬКУЛЯТОР\n" << endl;
-	cout << "1 | Сложить" << endl;
-	cout << "2 | Вычитать" << endl; 
-	cout << "3 | Умножать" << endl;
-	cout << "4 | Делить" << endl;
-	cout << "5 | Деление от остатка" << endl;
-	cout << "6 | Степень" << endl;
-	cout << "7 | Корень" << endl;
-	cout << "8 | Куб" << endl;
-	cout << "9 | Синус" << endl;
+	cout << "1  | Сложить" << endl;
+	cout << "2  | Вычитать" << endl; 
+	cout << "3  | Умножать" << endl;
+	cout << "4  | Делить" << endl;
+	cout << "5  | Деление от остатка" << endl;
+	cout << "6  | Степень" << endl;
+	cout << "7  | Корень" << endl;
+	cout << "8  | Куб" << endl;
+	cout << "9  | Синус" << endl;
 	cout << "10 | Косинус\n" << endl;
 	cout << "Ввод |<  "; cin >> UserNum;
 
@@ -223,10 +287,13 @@ int main() {
 		square();
 		break;
 	case 8:
+		degree3();
 		break;
 	case 9:
+		sinus();
 		break;
 	case 10:
+		cosinus();
 		break;
 	}
 
